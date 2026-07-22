6cc2e6cafa152af4a1c200e2537e31e52dfc119d
serdar özden <serdarozdenn@gmail.com>
2026-07-22 20:13:28 +0300

    added technical internet to nft guide to articles index

--- src/data/articles/index.js
+++ src/data/articles/index.js
@@ -1,7 +1,9 @@
 import { cadUnitsArticle } from './cad-units';
 import { nftGuideArticle } from './nft-guide';
+import { nftTechnicalArticle } from './nft-technical';
 
 export const articles = [
   cadUnitsArticle,
   nftGuideArticle,
+  nftTechnicalArticle,
 ];
